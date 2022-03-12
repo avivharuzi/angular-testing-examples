59f7bdaadcf0ad828359e7963d769c636f9a6a33
Aviv Haruzi <avivharuzi@gmail.com>
2022-03-12 11:02:21 +0200

    feat: 🎸 add counter module

---
+++ apps/angular-testing-examples/src/app/components/counter/counter.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { CounterComponent } from './counter.component';
+
+@NgModule({
+  declarations: [CounterComponent],
+  exports: [CounterComponent],
+})
+export class CounterModule {}
